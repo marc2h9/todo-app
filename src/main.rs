e405b6a5c3be0f69d055309cd4eeba869a343f29
marc2h9 <marcverstegen09@gmail.com>
2024-10-14 12:40:10 +0200

    Added Hello world test, Compiled program

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,25 @@ use std::io;
 
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use ratatui::{
+    text::Line,
     widgets::{Block, Paragraph}
 };
 
 fn main() -> io::Result<()> {
-    let mut terminal = ratatui::init()?;
+    let mut terminal = ratatui::init();
+    loop {
+        terminal.draw(|frame| {
+            frame.render_widget(
+                Paragraph::new("Hello, World!").block(Block::bordered().title_top(Line::from("Press q to quit").centered())),
+                frame.area()
+            )
+        })?;
+        if let Event::Key(key) = event::read()? {
+            if key.kind ==  KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                break;
+            }
+        }
+    }
+    ratatui::restore();
+    Ok(())
 }
