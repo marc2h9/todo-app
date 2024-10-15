81d1644cd150e0900b18dc84c680a07eb2766f37
marc2h9 <marcverstegen09@gmail.com>
2024-10-16 00:06:34 +0200

    Updated uihandler.rs

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,12 @@ pub mod uihandler;
 use std::io;
 
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
-use ratatui::{
-    widgets::Block,
-    widgets::Paragraph
-};
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
     loop {
         terminal.draw(|frame| {
-            uihandler::draw_test(frame);
+            uihandler::main(frame);
         })?;
         if let Event::Key(key) = event::read()? {
             if key.kind ==  KeyEventKind::Press && key.code == KeyCode::Char('q') {
--- src/uihandler.rs
+++ src/uihandler.rs
@@ -1,11 +1,55 @@
 use ratatui::{
+   prelude::*,
+   widgets::*,
    Frame,
-   widgets::{Block, Paragraph}
 };
 
-pub fn draw_test(frame: &mut Frame) {
+pub fn main(frame: &mut Frame) {
+    
+    // Top bar icon such as name and how to quit
+    let outer_layout_vertical = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![
+            Constraint::Percentage(50),
+            Constraint::Percentage(50),
+        ])
+        .split(frame.area());
+
+    let top_bar_layout_vertical = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![
+            Constraint::Percentage(10),
+            Constraint::Percentage(90),
+        ])
+        .split(outer_layout_vertical[0]);
+    
+    let top_bar_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(vec![
+            Constraint::Percentage(5),
+            Constraint::Percentage(10),
+            Constraint::Percentage(85),
+        ])
+        .split(top_bar_layout_vertical[0]);
+
     frame.render_widget(
-        Paragraph::new("Hello, World!").block(Block::bordered().title("Press Q to quit")),
-        frame.area()
+        Paragraph::new("Todo App")
+            .alignment(Alignment::Center)
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+            ),
+        top_bar_layout[0]
+
+    );
+
+    frame.render_widget(
+        Paragraph::new("Press \"Q\" to quit")
+            .alignment(Alignment::Center)
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+            ),
+        top_bar_layout[1]
     );
 }
